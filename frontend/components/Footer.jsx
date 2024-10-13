1f6a7a862d2f2a533337e64a0595225ef6ca5e6a
Habib-bhai <habibahmed918131@gmail.com>
2024-10-13 14:26:04 +0500

    home section done

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
-       <div>
-            <img src="../public/logo.png" alt="" className="w-10 h-10 rounded-2xl"/>
+       <div className="w-[100vw] h-[100vh] flex flex-col justify-center items-center">
+            {/* <img src="/logo.png" alt="" width={100} height={100} className="w-10 h-10 rounded-2xl"/> */}
        </div>
   )
 }
