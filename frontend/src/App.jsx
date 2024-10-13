4f60cddd57a6fecb2c8939064c971c802d07b8db
Habib-bhai <habibahmed918131@gmail.com>
2024-10-13 12:58:36 +0500

    add footer and contact route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Competition from "./pages/Competition"
 import CompetitionDetails from "./pages/CompetitionDetails"
+import Contact from "./pages/Contact"
+import Footer from "../components/Footer"
 
 
 function App() {
@@ -13,8 +15,10 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/Competition" element={<Competition/>}/>
+        <Route path="/contact" element={<Contact/>}/>
         <Route path="/Competition:competitionName" element={<CompetitionDetails/>}/>
     </Routes>
+    <Footer/>
     </>
   )
 }
